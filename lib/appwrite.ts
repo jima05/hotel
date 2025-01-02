86f5c5769b788074154132a5c61d40d9a5281b70
jima05 <jimabenjamin4@gmail.com>
2025-01-02 06:46:45 +0300

    New updates

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -133,3 +133,18 @@ export async function getProperties({ filter, query, limit} : { filter: string, 
     return [];
   }
 }
+
+// write function to get property by id
+export async function getPropertyById({ id }: { id: string }) {
+  try {
+    const result = await database.getDocument(
+      config.databaseId!,
+      config.propertiesCollectionId!,
+      id
+    );
+    return result;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+}
--- src/app/(root)/_layout.tsx
+++ src/app/(root)/_layout.tsx
@@ -19,6 +19,4 @@ export default function AppLayout(){
         return <Slot />;
     }
 
-    return <Slot />;
-
 }
